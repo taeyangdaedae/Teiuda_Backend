fa52b5f2d6dbcb680ce2b22a434ca686949f54b2
EC2 Default User <ec2-user@ip-172-31-36-13.ap-northeast-2.compute.internal>
2023-12-28 14:54:13 +0000

    create: database.py models.py, modified main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Path, HTTPException
+from pydantic import BaseModel
+from database import engineconn
+from models import Test
 
 app = FastAPI()
 
+engine = engineconn()
+session = engine.sessionmaker()
 
-@app.get("/hello")
-def hello():
-    return {"message": "안녕하세요 파이보"}
+
+class Item(BaseModel):
+    name : str
+    number : int
+
+@app.get("/")
+async def first_get():
+    example = session.query(Test).all()
+    return example
